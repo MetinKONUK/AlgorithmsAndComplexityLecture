21cf7eebaec2457016618006afbff56cc293be5c
Metin KONUK <metinkonuk@hotmail.com>
2022-03-10 14:17:24 +0300

    add SumOfArrayOfNNumbersDivideByTwo recursive algorithm -> O(LogN) and it's time and space complexity descriptions

--- Week3/main.cpp
+++ Week3/main.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int SumOfOneToN(int);
 int SumOfArrayOfNNumbers(vector<int>, int);
+int SumOfArrayOfNNumbersDivideByTwo(vector<int>, int, int);
 
 int main() {
 	vector<int> A = { 1, 3, 4, 5, 5, 13 };
 	//cout << SumOfOneToN(10) << endl;
-	cout << SumOfArrayOfNNumbers(A, A.size()-1) << endl;
+	//cout << SumOfArrayOfNNumbers(A, A.size()-1) << endl;
+	cout << SumOfArrayOfNNumbersDivideByTwo(A, 0, A.size()-1) << endl;
+
 	return 0;
 }
